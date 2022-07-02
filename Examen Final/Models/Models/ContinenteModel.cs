ce1da55afaed3720112ffd1fb321b4850f495756
ohinestroza <52361012+ohinestroza@users.noreply.github.com>
2022-07-02 11:06:23 -0600

    Add files via upload

---
+++ Examen Final/Models/Models/ContinenteModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class ContinenteModel
+    {
+        public int id_continente { get; set; }
+        public string nombre_continente { set; get; }
+    }
+}
